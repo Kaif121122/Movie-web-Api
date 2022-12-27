d5429400987f40a608b246d0142b2a0c895c3d10
Kaif <kaif9987699@gmail.com>
2022-12-27 07:05:47 +0000

    ok

---
+++ src/components/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Search from './Search'
+import Movie from './Movie'
+
+
+
+const Home = () => {
+
+  return (<div className='container'>
+     <Search/>
+      <Movie/>
+      
+     
+      </div>
+
+  )
+}
+
+export default Home
